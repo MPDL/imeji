d8418a910e27e3b10bbdb0f548bcb75536f9e108
bastiensaquet <saquet@mpdl.mpg.de>
2016-10-28 15:41:34 +0200

    Remove old rdf export

--- src/main/java/de/mpg/imeji/logic/export/ExportFactory.java
+++ src/main/java/de/mpg/imeji/logic/export/ExportFactory.java
@@ -5,17 +5,11 @@ import java.util.Map;
 import org.apache.http.client.HttpResponseException;
 
 import de.mpg.imeji.logic.export.format.Export;
-import de.mpg.imeji.logic.export.format.JenaExport;
 import de.mpg.imeji.logic.export.format.SitemapExport;
 import de.mpg.imeji.logic.export.format.ZIPExport;
 import de.mpg.imeji.logic.export.format.explain.ExplainExport;
 import de.mpg.imeji.logic.export.format.explain.MetadataExplainExport;
 import de.mpg.imeji.logic.export.format.explain.SearchExplainExport;
-import de.mpg.imeji.logic.export.format.rdf.RDFAlbumExport;
-import de.mpg.imeji.logic.export.format.rdf.RDFCollectionExport;
-import de.mpg.imeji.logic.export.format.rdf.RDFExport;
-import de.mpg.imeji.logic.export.format.rdf.RDFImageExport;
-import de.mpg.imeji.logic.export.format.rdf.RDFProfileExport;
 
 /**
  * Factory for export
@@ -40,13 +34,7 @@ public class ExportFactory {
     Export export = null;
     String format = Export.getParam(params, "format");
     String type = Export.getParam(params, "type");
-    if (format == null || "".equals(format)) {
-      export = buildRdf(type);
-    } else if ("rdf".equals(format)) {
-      export = buildRdf(type);
-    } else if ("jena".equals(format)) {
-      export = new JenaExport();
-    } else if ("sitemap".equals(format)) {
+    if ("sitemap".equals(format)) {
       export = new SitemapExport();
     } else if ("zip".equals(format)) {
       export = new ZIPExport(type);
@@ -60,25 +48,6 @@ public class ExportFactory {
     return export;
   }
 
-  /**
-   * Factory for {@link RDFExport}
-   *
-   * @param type
-   * @return
-   * @throws HttpResponseException
-   */
-  private static RDFExport buildRdf(String type) throws HttpResponseException {
-    if ("image".equalsIgnoreCase(type)) {
-      return new RDFImageExport();
-    } else if ("collection".equalsIgnoreCase(type)) {
-      return new RDFCollectionExport();
-    } else if ("album".equalsIgnoreCase(type)) {
-      return new RDFAlbumExport();
-    } else if ("profile".equals(type)) {
-      return new RDFProfileExport();
-    }
-    throw new HttpResponseException(400, "Type " + type + " is not supported.");
-  }
 
   /**
    * Factory for {@link ExplainExport}
