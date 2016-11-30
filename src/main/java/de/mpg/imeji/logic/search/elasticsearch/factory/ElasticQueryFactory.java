13d4e8a4275555c0924038873207eabe632b73a8
bastiensaquet <saquet@mpdl.mpg.de>
2016-11-30 14:48:04 +0100

    Change elastic analysers for names

--- src/main/java/de/mpg/imeji/logic/search/elasticsearch/factory/ElasticQueryFactory.java
+++ src/main/java/de/mpg/imeji/logic/search/elasticsearch/factory/ElasticQueryFactory.java
@@ -698,8 +698,8 @@ public class ElasticQueryFactory {
    */
   private static QueryBuilder matchFieldQuery(ElasticFields field, String value) {
     if (field == ElasticFields.ALL) {
-      return QueryBuilders.queryStringQuery(value + " " + ElasticFields.NAME.field() + ":" + value)
-          .quoteAnalyzer("standard");
+      return QueryBuilders
+          .queryStringQuery(value + " " + ElasticFields.NAME.field() + ".suggest:" + value);
     }
     return QueryBuilders.queryStringQuery(field.field() + ":" + value);
   }
