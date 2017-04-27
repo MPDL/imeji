5d5b49e53f872eb707c613ce24d197ec30ce7342
jandura <jandura@w306.mucam.mpg.de>
2017-04-27 13:51:23 +0200

    Fixed Tests and OSD bug

--- src/test/java/de/mpg/imeji/test/logic/service/SuperServiceTest.java
+++ src/test/java/de/mpg/imeji/test/logic/service/SuperServiceTest.java
@@ -3,20 +3,7 @@ package de.mpg.imeji.test.logic.service;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
-<<<<<<< HEAD
-import util.JenaUtil;
-=======
-import de.mpg.imeji.exceptions.ImejiException;
-import de.mpg.imeji.logic.collection.CollectionService;
-import de.mpg.imeji.logic.item.ItemService;
-import de.mpg.imeji.logic.vo.CollectionImeji;
-import de.mpg.imeji.logic.vo.Item;
-import de.mpg.imeji.logic.vo.User;
-import de.mpg.imeji.logic.vo.factory.CollectionFactory;
-import de.mpg.imeji.logic.vo.factory.ImejiFactory;
-import de.mpg.imeji.util.ImejiTestResources;
 import de.mpg.imeji.util.JenaUtil;
->>>>>>> branch 'imeji4' of https://github.com/MPDL/imeji.git
 
 /**
  * Created by vlad on 15.04.15.
