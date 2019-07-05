7ffa70a09e0578d099065297a679fe5e91243c5e
helkv <helk@mpdl.mpg.de>
2019-07-05 10:18:59 +0200

    #1104 Add new test classes to SuiteClass
    -> So that new test classes are being executed they have to be added to
    the corresponding suite classes for now.
    - Add CollectionServiceCreateTest and StatementServiceUpdateTest to
    ServiceTestSuite

--- src/test/java/de/mpg/imeji/test/logic/service/ServiceTestSuite.java
+++ src/test/java/de/mpg/imeji/test/logic/service/ServiceTestSuite.java
@@ -3,9 +3,11 @@ package de.mpg.imeji.test.logic.service;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import de.mpg.imeji.testimpl.logic.service.CollectionServiceCreateTest;
 import de.mpg.imeji.testimpl.logic.service.ContentServiceTest;
 import de.mpg.imeji.testimpl.logic.service.ShareServiceTest;
 import de.mpg.imeji.testimpl.logic.service.StatementServiceTest;
+import de.mpg.imeji.testimpl.logic.service.StatementServiceUpdateTest;
 import de.mpg.imeji.testimpl.logic.service.UserServiceTest;
 import de.mpg.imeji.testimpl.logic.service.UsergroupServiceTest;
 
@@ -13,6 +15,7 @@ import de.mpg.imeji.testimpl.logic.service.UsergroupServiceTest;
 @Suite.SuiteClasses({
     //CollectionServiceTest.class, 
     ShareServiceTest.class, StatementServiceTest.class, UsergroupServiceTest.class, UserServiceTest.class, ContentServiceTest.class,
+    CollectionServiceCreateTest.class, StatementServiceUpdateTest.class
     //ItemServiceTest.class
 })
 
