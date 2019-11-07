3e8ffecc8f6f6be7282115826fda909a252d5dd8
helkv <helk@mpdl.mpg.de>
2019-11-07 10:54:12 +0100

    #1104 Fix ConcurrentModificationException -> Reworked waiting mechanism
    -> Fix for ConcurrentModificationException in
    ConcurrencyUtil.waitForThreadsToComplete()
    -> ConcurrentModificationException is thrown if another
    threadPoolExecutor is added to the list, while
    waitForThreadsToComplete() is already iterating over it.
    
    - Reworked ConcurrencyUtil.waitForThreadsToComplete(): Catch the
    ConcurrentModificationException and run waitForThreadsToComplete() again
    to also check the new added threadPool

--- src/test/java/de/mpg/imeji/util/ConcurrencyUtil.java
+++ src/test/java/de/mpg/imeji/util/ConcurrencyUtil.java
@@ -3,7 +3,11 @@ package de.mpg.imeji.util;
 import static org.awaitility.Awaitility.await;
 
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
 import java.util.concurrent.ThreadPoolExecutor;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Utility/Helper class for concurrency utilities.
@@ -12,6 +16,8 @@ import java.util.concurrent.ThreadPoolExecutor;
  *
  */
 public final class ConcurrencyUtil {
+
+  private static final Logger LOGGER = LogManager.getLogger(ConcurrencyUtil.class);
 
   private ConcurrencyUtil() {
     //Should not be instantiated.
@@ -23,9 +29,19 @@ public final class ConcurrencyUtil {
    * @param threadPoolExecutors Collection of {@code ThreadPoolExecutors} to wait for.
    */
   public static void waitForThreadsToComplete(Collection<ThreadPoolExecutor> threadPoolExecutors) {
-    for (ThreadPoolExecutor threadPoolExecutor : threadPoolExecutors) {
-      waitForThreadsToComplete(threadPoolExecutor);
+    LOGGER.info("Wait for Threads to compelete...");
+
+    try {
+      LOGGER.info("Iterating over " + threadPoolExecutors.size() + " ThreadPoolExecutors.");
+      for (ThreadPoolExecutor threadPoolExecutor : threadPoolExecutors) {
+        waitForThreadsToComplete(threadPoolExecutor);
+      }
+    } catch (ConcurrentModificationException e) {
+      LOGGER.info("Catched a ConcurrentModificationException! -> Restart iterating over the ThreadPoolExecutors:");
+      waitForThreadsToComplete(threadPoolExecutors);
     }
+
+    LOGGER.info("Threads compeleted.");
   }
 
   /**
